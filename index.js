6dff70c577b61bd06eaf72f249acac3c314b10bc
Adarsh K <adarshbhatk@gmail.com>
2024-06-21 20:46:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,36 +5,49 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Facebook@1997",
+  port: 5432
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "world",
-    password: "Facebook@1997",
-    port: 5432
-  });
-  await db.connect();
   const data = await db.query("SELECT country_code from visited_countries");
   if(data.rows) {
       const country_data = data.rows;
       const countries = [];
       country_data.forEach((item) => {countries.push(item.country_code)});
       const total = countries.length;
-      // console.log(total);
-      // console.log(countries);
-      await db.end();
       res.render("index.ejs", {
         total: total,
         countries: countries
       });
     } else {
       console.error("Error executing query", err.stack);
-      await db.end();
       res.status(404).send("Error executing query");
     }
+  });
+
+  app.post("/add", async (req, res) => {
+    const countryName = req.body.country;
+    const data = await db.query("SELECT country_code from countries WHERE country_name = $1", [countryName]);
+    if(data.rows) {
+      const countryCode = data.rows[0].country_code;
+      const idArray = await db.query("SELECT id from visited_countries");
+      const id = (idArray.rows[idArray.rows.length - 1].id) + 1;
+      await db.query("INSERT INTO visited_countries (id, country_code) VALUES ($1, $2)", [id, countryCode]);
+    } else {
+      console.error("Error executing query", err.stack);
+      res.status(404).send("Error executing query");
+    }
+    res.redirect("/");
   });
 
 app.listen(port, () => {
